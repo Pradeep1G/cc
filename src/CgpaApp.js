5d0513d3db36a166d6dcc7a3c20693980d1d6020
Geddada Pradeep <geddadavenkatapradeep@gmail.com>
2023-06-15 23:12:33 +0530

    commit

--- src/CgpaApp.js
+++ src/CgpaApp.js
@@ -79,7 +79,7 @@ function CgpaApp(props) {
 
 
     for(var j=0; j<marks_list.length; j++){
-      if(allData[3][j]===10){
+      if(allData[3][j]==="10"){
         subjectpartialcredits.push((Number.parseInt((Number.parseInt(marks_list[j]*2)/10))+1)*(allData[2][j]))
 
       }else{
